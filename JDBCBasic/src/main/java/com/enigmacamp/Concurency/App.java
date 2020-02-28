b4bda5d2c557b5baebfb1014fd65161a6e04f307
aditya.saputra <adityasaputra111298@gmail.com>
2020-02-28 15:42:22 +0700

    add synchronized

--- JDBCBasic/src/main/java/com/enigmacamp/Concurency/App.java
+++ JDBCBasic/src/main/java/com/enigmacamp/Concurency/App.java
@@ -1,22 +1,44 @@
 package com.enigmacamp.Concurency;
 
 public class App {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("BEFORE START");
-        MyThread thread1 = new MyThread(1, 1000);
-        thread1.setName("Thread ");
-        thread1.start();
-
+    public static void main(String[] args) throws Exception {
+        // System.out.println("BEFORE START");
+        // thread1.setName("Thread ");
         // MyRunnable runnable = new MyRunnable(101, 200);
-        // Thread thread2 = new Thread(runnable);
+        // MyThread thread1 = new MyThread(1, 100);
+        Counter counter = new Counter(0);
+
+        Thread thread1 = new MyThread(counter, 100000);
+        thread1.setName("Thread1");
+
+        Thread thread2 = new MyThread(counter, 100000);
+        thread1.setName("Thread2");
+
+
+        Thread[] threads ={thread1,thread2};
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+            // System.out.println(thread.getName()+ " done.");
+        }
+        System.out.println("Counter: " + counter.getValue());
+        
+
+
+        // thread1.start();
         // thread2.start();
 
 
-        Thread.sleep(1000000);
-        thread1.interrupt();
 
 
-        System.out.println("AFTER START");
+
+
+        // Thread.sleep(1000000);
+        // thread1.interrupt();
+
     }
 
 }
